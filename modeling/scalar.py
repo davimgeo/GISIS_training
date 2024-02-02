cc4781216692eaf680e642563439de5508fe818d
davimgeo <davi.joestar02@mgmail.com>
2024-02-02 01:29:47 -0300

    teste

--- modeling/scalar.py
+++ modeling/scalar.py
@@ -9,9 +9,26 @@ class Wavefield_1D():
 
         # TODO: read parameters from a file
 
-        self.nt = 1001
-        self.dt = 1e-3
-        self.fmax = 30.0
+        path = r'C:\Users\malum\Desktop\DAVI\GISIS_training\parameters.txt'
+        parameters = np.loadtxt(path)
+
+        self.nt = int(parameters[0]) # 1001.0
+        self.dt = parameters[1] # 1e-3
+        self.fmax = int(parameters[2]) # 30.0
+
+        self.nx =  1001 #int(parameters[3]) # 1001.0
+        self.dz = int(parameters[4]) # 5.0
+
+        self.depth = np.arange(self.dz)*self.dz
+        self.times = np.arange(self.nt)*self.dt
+
+        self.interfaces = np.array([1000, 2000, 3000, 4000])
+        self.velocities = np.array([1500, 2000, 2500, 3000, 3500])
+
+        self.model = self.velocities[0]*np.ones(self.dz)
+
+        self.z_src = np.array([100, 200, 300])
+        self.z_rec = np.array([2500, 3500, 4500])
 
     def get_type(self):
         print(self._type)
@@ -28,20 +45,21 @@ class Wavefield_1D():
 
     def plot_wavelet(self):
        
-        t = np.arange(self.nt)*self.dt
+        fig, ax = plt.subplots(num = "Velocity Model", figsize = (10, 5), clear = True)
 
-        fig, ax = plt.subplots(figsize = (10, 5), clear = True)
-
-        ax.plot(t, self.wavelet)
-        ax.set_title("Wavelet", fontsize = 18)
-        ax.set_xlabel("Time [s]", fontsize = 15)
-        ax.set_ylabel("Amplitude", fontsize = 15) 
+        ax.plot(self.model, self.interfaces)
+        ax.set_title("Velocity Model", fontsize = 18)
+        ax.set_xlabel("Velocity [m/s]", fontsize = 15)
+        ax.set_ylabel("Depth [m]", fontsize = 15) 
         
-        ax.set_xlim([0, np.max(t)])
+        ax.set_xlim([0, (self.nt-1)*self.dt])
         
         fig.tight_layout()
         plt.show()
-
+    
+    def set_model(self):
+        for layerId, interface in enumerate(self.interfaces):
+            self.model[int(interface/self.dz):] = self.velocities[layerId+1]   
 
 class Wavefield_2D(Wavefield_1D):
     
